62efd148729bf452c16fb801fa8fda8546ee8138
jojochr <jojochr@users.noreply.github.com>
2024-12-01 20:16:25 +0100

    Initialized AdventOfCode.sln and developed a Solution for Day 1

---
+++ Day1/InputReader.cs
@@ -0,0 +1,41 @@
+using System.Reflection;
+using System.Text;
+
+namespace Day1 {
+    public static class InputReader {
+        public static (int[] leftNumbersRaw, int[] rightNubmersRaw) GetDay1Input() {
+            int[] leftNumbersRaw = new int[1_000];
+            int[] rightNumbersRaw = new int[1_000];
+
+
+
+            using FileStream fileStream = new(Path.Combine(Directory.GetCurrentDirectory(), "Day1-Input.txt"), FileMode.Open, FileAccess.ReadWrite);
+            using StreamReader streamReader = new StreamReader(fileStream, Encoding.UTF8, true);
+
+            if(streamReader is null) {
+                throw new Exception("Failed to open input file");
+            }
+
+            int index = 0;
+            while(streamReader.EndOfStream == false) {
+                string inputLine = streamReader.ReadLine() ?? string.Empty;
+                string[] inputsElements = inputLine.Split(separator: ' ', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries);
+
+                if(inputsElements.Length != 2) {
+                    throw new Exception($"Got more or less elements than expected on one line. Number of Elements: \"{inputsElements.Length}\"");
+                }
+
+                leftNumbersRaw[index] = int.Parse(inputsElements[0]);
+                rightNumbersRaw[index] = int.Parse(inputsElements[1]);
+
+                index++;
+
+                if(index == 1_000) {
+                    break;
+                }
+            }
+
+            return (leftNumbersRaw, rightNumbersRaw);
+        }
+    }
+}
