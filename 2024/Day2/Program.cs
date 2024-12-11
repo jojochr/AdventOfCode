c385ca87cf3c416d3026851dd3c7c18440676700
jojochr <jojochr@users.noreply.github.com>
2024-12-11 22:41:06 +0100

    Added solution for Day 2 Part 1

--- 2024/Day2/Program.cs
+++ 2024/Day2/Program.cs
@@ -1,3 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Collections.Immutable;
+using System.Diagnostics;
+using Day2;
 
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Start of Solution for Day2:");
+
+int safeReports = 0;
+const int MIN_STATUS_CODE_DIFFERENCE = 1;
+const int MAX_STATUS_CODE_DIFFERENCE = 3;
+ImmutableList<ImmutableArray<ushort>> rawReactorCodes = InputReader.GetDay2Input();
+Stopwatch watch = new();
+
+foreach(ImmutableArray<ushort> statusValues in rawReactorCodes) {
+    if(true == StatusCodeChecker.CheckStatusCode(statusValues, MIN_STATUS_CODE_DIFFERENCE, MAX_STATUS_CODE_DIFFERENCE)) {
+        safeReports++;
+    }
+}
+
+Console.WriteLine($"Result for Day 1: \"{safeReports}\"");
+
+// Todo: Do Day 2 Part 2 :)
+// Console.WriteLine();
+// Console.WriteLine("Starting Part 2:");
+// Console.WriteLine($"Result for Part 2 of Day 1 is: \"{similarityScore}\"");
+
+Console.ReadKey();
