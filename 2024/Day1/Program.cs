bc041e525b1d9f3727d2f3ca7103abb3fe20eabd
jojochr <jojochr@users.noreply.github.com>
2024-12-19 18:51:54 +0100

    Cleaned up Solution

--- 2024/Day1/Program.cs
+++ 2024/Day1/Program.cs
@@ -1,21 +1,23 @@
 ﻿using Day1;
+using Shared;
 
-Console.WriteLine("Hello, World!");
 Console.WriteLine("This is my Solution for Day 1 :)");
+Console.WriteLine();
 
 (int[] leftArray, int[] rightArray) = InputReader.GetDay1Input();
 
+// Sort both arrays asynchronously
 Task.WaitAll(Task.Run(leftArray.BubbleSortThis), Task.Run(rightArray.BubbleSortThis));
 
 if(leftArray.Length != rightArray.Length) {
     throw new Exception("Lists did not have the same length as defined in Task");
 }
 
+// Evaluate the differences
 long differenceBetweenLists = 0;
-
 for(int i = 0; i < leftArray.Length; i++) {
     differenceBetweenLists += Math.Abs(leftArray[i] - rightArray[i]);
 }
 
-Console.WriteLine($"Result for the Task of Day 1 = \"{differenceBetweenLists}\"");
+Console.WriteLine($"Result for Day 1: \"{differenceBetweenLists}\"");
 Console.ReadKey();
